c1e004cf2178c493a97a8e390861e6b59222a49a
ArmenVardanyan <varmen83@gmail.com>
2014-12-02 18:14:52 +0400

    HelpBundle

--- DependencyInjection/YitHelpExtension.php
+++ DependencyInjection/YitHelpExtension.php
@@ -6,13 +6,14 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 
 /**
  * This is the class that loads and manages your bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class YitHelpExtension extends Extension
+class YitHelpExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritDoc}
@@ -30,7 +31,7 @@ class YitHelpExtension extends Extension
      * {@inheritDoc}
      */
     public function prepend(ContainerBuilder $container)
-    {
+    { 
         // get all Bundles
         $bundles = $container->getParameter('kernel.bundles');
 
